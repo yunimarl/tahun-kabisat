410ce1b2e2ff8673f011db888f01abe1e46e89ad
yunimarl <61624725+yunimarl@users.noreply.github.com>
2020-07-09 13:18:13 +0700

    Add files via upload

---
+++ kabisat.py
@@ -0,0 +1,11 @@
+tahun = int(input())
+if (tahun % 4) == 0:
+    if (tahun % 100) == 0:
+        if (tahun % 400) == 0:
+            print ("Tahun Kabisat")
+        else:
+            print ("Bukan Tahun Kabisat")
+    else:
+        print ("Tahun Kabisat")
+else:
+    print ("Bukan Tahun Kabisat")
